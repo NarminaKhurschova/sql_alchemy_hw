2ca7a1d54f0b18b0ad13bf8de11f7a4e5d92dd7e
narmina <namira.lir@gmail.com>
2022-10-25 15:55:25 +0300

    Commit 1

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ import json
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"
 
-db = SQLAlchemy(app)
+
+db: SQLAlchemy = SQLAlchemy(app)
 
 
 class User(db.Model):
@@ -19,6 +20,9 @@ class User(db.Model):
     role = db.Column(db.String)
     phone = db.Column(db.String)
 
+    orders = db.relationship("Order", foreign_keys=["customer_id"])
+    # offers = db.relationship("Offer")
+
 
 class Order(db.Model):
     __tablename__ = "order"
@@ -30,36 +34,50 @@ class Order(db.Model):
     end_date = db.Column(db.Date)
     address = db.Column(db.String)
     price = db.Column(db.Integer)
-    customer_id = db.Column(db.Integer, db.ForeignKey("user.id"))
-    executor_id = db.Column(db.Integer, db.ForeignKey("user.id"))
 
-    user = db.relationship("User")
+    customer_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    customer_user = db.relationship("User", foreign_keys=[customer_id])
+
+    executor_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    executor_user = db.relationship("User", foreign_keys=[executor_id])
 
 
 class Offer(db.Model):
     __tablename__ = "offer"
 
     id = db.Column(db.Integer, primary_key=True)
-    order_id = db.Column(db.Integer, db.ForeignKey("id"))
+    order_id = db.Column(db.Integer, db.ForeignKey("order.id"))
     executor_id = db.Column(db.Integer, db.ForeignKey("order.executor_id"))
 
-    order = db.relationship('Order')
+    order = db.relationship("Order")
 
 
-db.create_all()
+with app.app_context():
+    db.create_all()
 
-users = []
 
 with open("users.json") as file:
     users_from_json = json.load(file)
 
     for user in users_from_json:
-        user.id = User(id=user.id, first_name=user.first_name, last_name=user.last_name, age=user.age, email=user.email,
-                       role=user.role, phone=user.phone)
-        users.append(user.id)
+        user_s = User(id=user["id"], first_name=user["first_name"], last_name=user["last_name"], age=user["age"],
+                  email=user["email"], role=user["role"], phone=user["phone"])
+        db.session.addl(user_s)
+        db.session.commit()
 
-db.session.add_all(users)
-db.session.commit()
+with open("orders.json") as file_o:
+    orders_from_json = json.load(file_o)
+
+    for order in orders_from_json:
+        order_s = Order(id=order["id"], name=order["name"], description=order["description"],
+                        start_date=order["start_date"], end_date=order["end_date"], address=order["address"],
+                        price=order["price"], customer_id=order["customer_id"], executor_id=order["executor_id"])
+
+with open("offers.json") as file_of:
+    offers_from_json = json.load(file_of)
+
+    for offer in offers_from_json:
+        offer_s = Offer(id=offer["id"], order_id=offer["order_id"], executor_id=offer["executor_id"])
 
 
 if __name__ == '__main__':
